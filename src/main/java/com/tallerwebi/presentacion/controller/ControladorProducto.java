26b8fd3d47ea17ce33c51924e67d0d2643864594
Santi Gismondi <129218589+santigismondi@users.noreply.github.com>
2026-05-21 19:31:49 -0300

    Revert "nose que es"
    This reverts commit e56d889358912834eedd5d169d43d205ecef7bed.

--- src/main/java/com/tallerwebi/presentacion/controller/ControladorProducto.java
+++ src/main/java/com/tallerwebi/presentacion/controller/ControladorProducto.java
@@ -1,12 +1,14 @@
 package com.tallerwebi.presentacion.controller;
 
+import com.tallerwebi.presentacion.dto.CategoriaDto;
 import com.tallerwebi.dominio.entity.Producto;
 import com.tallerwebi.dominio.interfaces.ServicioCategoria;
 import com.tallerwebi.dominio.interfaces.ServicioProducto;
-import com.tallerwebi.presentacion.dto.CategoriaDto;
+
 import com.tallerwebi.presentacion.dto.ProductoDto;
 import java.util.List;
 import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -25,8 +27,7 @@ public class ControladorProducto {
   @Autowired
   public ControladorProducto(
     ServicioProducto servicioProducto,
-    ServicioCategoria servicioCategoria
-  ) {
+    ServicioCategoria servicioCategoria) {
     this.servicioProducto = servicioProducto;
     this.servicioCategoria = servicioCategoria;
   }
