29c538a257ac88b6e7f8d647ab2c07b79ce59fbc
Santi Gismondi <129218589+santigismondi@users.noreply.github.com>
2026-05-28 18:46:45 -0300

    Revert "RunApp.sh"
    This reverts commit 2849d20dc6792695915bfb80225ca7701e44180a.

--- runApp.sh
+++ runApp.sh
@@ -1,1 +1,8 @@
-docker start mysql-container
+#!/bin/bash
+# Build the Maven project (skip tests for faster dev)
+mvn clean package -DskipTests
+# Start Docker containers, building images if necessary
+docker-compose up --build -d
+# Show Jetty logs (Ctrl+C to exit)
+echo "Jetty container logs:"
+docker logs -f tallerwebi-jetty
