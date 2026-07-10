e4795183ebe54765d4448d6a4f9461c8e0c4d7ae
carlosrizzetti <carlosrizzetti@gmail.com>
2026-07-09 21:59:11 -0300

    test2

---
+++ src/test/java/com/tallerwebi/dominio/ProductoFinalTest.java
@@ -0,0 +1,31 @@
+package com.tallerwebi.dominio;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+import com.tallerwebi.dominio.entity.ProductoFinal;
+import com.tallerwebi.dominio.entity.Receta;
+import java.math.BigDecimal;
+import org.junit.jupiter.api.Test;
+
+public class ProductoFinalTest {
+
+  @Test
+  public void queSePuedanManejarPropiedades() {
+    ProductoFinal pf = new ProductoFinal();
+    pf.setId(10L);
+    pf.setNombre("Milanesa");
+    pf.setPrecio(BigDecimal.valueOf(1500));
+
+    Receta receta = new Receta();
+    receta.setId(5L);
+    pf.setReceta(receta);
+
+    assertThat(pf.getId(), equalTo(10L));
+    assertThat(pf.getNombre(), equalTo("Milanesa"));
+    assertThat(pf.getPrecio(), equalTo(BigDecimal.valueOf(1500)));
+    assertThat(pf.getReceta(), notNullValue());
+    assertThat(pf.getReceta().getId(), equalTo(5L));
+  }
+}
